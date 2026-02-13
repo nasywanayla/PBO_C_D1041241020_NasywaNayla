2a5d65c51820c1ef90c84db895f61430b0f5410c
nasywanayla <nasywanayla06@gmail.com>
2026-02-13 21:43:02 +0700

    Update Java

---
+++ p022WorkingVariable.java
@@ -0,0 +1,17 @@
+/*
+NIM : D1041241020
+Nama : Nasywa Nayla
+Program untuk variabel
+*/
+public class p022WorkingVariable {
+    public static void main(String[] args) {
+        //Deklarasi dan inisialisasi variabel
+        int angka = 10;
+        //Menampilkan nilai awal
+        System.out.println("Nilai awal angka: " + angka);
+        //Mengubah nilai variabel
+        angka = 25;
+        //Menampilkan nilai setelah variabel
+        System.out.println("Nilai angka setelah diubah: " + angka);
+    }
+}
---
+++ p023PrimitiveData.java
@@ -0,0 +1,26 @@
+/*
+NIM : D1041241020
+Nama : Nasywa Nayla
+Program untuk Primitive Data
+*/
+public class p023PrimitiveData {
+    public static void main (String[] args) {
+        //Tipe data bulangan bulat
+        int umur = 20;
+        long populasi = 8000000000L;
+        //Tipe data bilangan desimal
+        float tinggi = 170.5f;
+        double ipk = 3.75;
+        //Tipe data karakter
+        char nilaiHuruf = 'A';
+        //Tipe data logika
+        boolean lulus = true;
+        //Menampilkan nilai variabel
+        System.out.println("Umur                        : " + umur);
+        System.out.println("Populasi                    : " + populasi);
+        System.out.println("Tinggi Badan                : " + tinggi);
+        System.out.println("IPK                         : " + ipk);
+        System.out.println("Nilai Huruf                 : " + nilaiHuruf);
+        System.out.println("Status Lulus                : " + lulus);
+    }
+}
